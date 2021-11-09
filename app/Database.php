bb4dd6fc5560e43d99a87c13355710e3666fe927
Robson Lucas <64038141+robsonad18@users.noreply.github.com>
2021-11-09 15:05:37 -0300

    Update Database.php

--- app/Database.php
+++ app/Database.php
@@ -18,7 +18,7 @@ abstract class Database
     public static function get()
     {
         try {
-            $pdo = new \PDO('mysql:host=localhost;dbname=argos', "root", "", [
+            $pdo = new \PDO('mysql:host=localhost;dbname=test', "root", "", [
                 PDO::MYSQL_ATTR_INIT_COMMAND  => "SET NAMES 'utf8mb4'",
                 PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC
